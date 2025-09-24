25979d7fcb82f9e88e100e5a18184cb740d9d39a
kaboom771 <savateev-05@mail.ru>
2025-09-24 20:11:58 +0300

    0 lab

--- 00_distance.py
+++ 00_distance.py
@@ -16,8 +16,11 @@ distances = {}
 
 # TODO здесь заполнение словаря
 
+for city1, coordintaion1 in sites.items(): # возвращает в city1 - города по списку, coordination1 - координаты корода (x,y)
+    distances[city1] = {} # словарь городов
+    for city2, coordination2 in sites.items():
+        if city1 != city2:
+            distance = ((coordintaion1[0] - coordination2[0]) ** 2 + (coordintaion1[1] - coordination2[1]) ** 2) ** 0.5
+            distances[city1][city2] = round(distance, 3)
+
 print(distances)
-
-
-
-
